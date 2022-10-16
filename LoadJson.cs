87500ddd497c217155065605524b6b242e22652c
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-16 22:31:55 +0530

    Stock Account Management System

---
+++ LoadJson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonDemo
+{
+    public class LoadJson
+    {
+        public Model Read(String path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Model>(json);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+
+            }
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDemo
+{
+    public class Model
+    {
+        public List  <StockMarket> stockMarket;
+
+    }
+    public class StockMarket
+    {
+        public string shareName { get; set; }
+        public int numberOfShare { get; set; }
+        public int sharePrice { get; set; }
+    }
+
+}
